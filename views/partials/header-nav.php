3a17f4e5b7cc5c83f62fdb997487e7727d9920f5
kapufskipauw <rosarhode@gmail.com>
2026-01-08 12:29:02 +0100

    refactor: Updated to PHP
    Created partials map with header-nav file

---
+++ views/partials/header-nav.php
@@ -0,0 +1,31 @@
+ <header class="header-logo-navigation">
+        <div class="header-logo">
+            <p>
+                Techdames
+            </p>
+        </div>
+        <div class="header-nav">
+            <ul>
+                <li>
+                    <a href="/">
+                    Home
+                    </a>
+                </li>
+                <li >
+                    <a href="../biografie.php">
+                    Biografie
+                    </a>
+                </li>
+                <li >
+                    <a href="../erkenning.php">
+                    Erkenning
+                    </a>
+                </li>    
+                <li >
+                    <a href="../bronnen.php">
+                    Bronnen 
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </header>
