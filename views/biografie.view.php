4bb782e238020732e801a3366096ed4be31a9352
kapufskipauw <rosarhode@gmail.com>
2026-01-09 18:46:38 +0100

    Add: Updated biografie page
    Added content and some links to external websites.

--- views/biografie.view.php
+++ views/biografie.view.php
@@ -7,20 +7,33 @@
 <main>
     <div class="introduction-container">
         <div class="text-introduction-container">
-            <h2 class="text-introduction-title">
-                Biografie van Ada Lovelace
-            </h2>
             <p class="paragraph-styling">
-                abracadabra
+                Ada Lovelace werd geboren op 10 december 1815 in Piccadilly Terrace, Middlesex (nu: Londen) en overleed op 27 november 1852 in Marylebone, London.
+                Ada was een Engelse wiskundige die samenwerkte met <a href="https://www.britannica.com/biography/Charles-Babbage">Charles Babbage</a>, voor zijn prototype digitale computer schreef zij een programma. Ze is benoemd
+                tot de eerste computerprogrammeur.<br><br>
+                Ada was de dochter van de bekende dichter<a href="https://www.britannica.com/biography/Lord-Byron-poet">Heer Byron</a> en Annabella Milbanke, een vrouw met een sterke interesse in wiskunde en logica.
+                Kort na de geboorte van Lovelace gaan haar ouders uit elkaar. Haar vader verliet Engeland voorgoed, dus ze leerde hem nooit persoonlijk kennen. <br><br>Haar moeder stimuleerde haar in de exacte wetenschappen, naar horen en zeggen:
+                'Om te voorkomen dat Ada, net als haar vader, te emotioneel of “dromerig” zou worden.’ Ada bleek al jong talent te hebben voor wiskunde en natuurkunde. Ze kreeg privéles door verschillende tutors, waaronder Augustus De Morgan, de eerste professor wiskunde aan de Universiteit van Londen. Op 8 juli 1835 trouwende ze met William King, de 8ste baron King, die in 1839 tot graaf werd benoemd, waardoor Ada de gravin van Lovelace werd. <br><br>
+Lovelace raakte sinds 1833 geinteresseerd in Babbage’s machines nadat zij door 
+Mary Somerville en  een bekende wiskundige en wetenschapscommunicator werd voorgesteld aan Charles Babbage. Charles was de ontwerper van de <a href=”https://www.britannica.com/technology/Analytical-Engine”> Analytical Engine </a>, een mechanische machine die wordt beschouwd als een voorloper van de moderne computer.<br><br>
+
+
+Ada’s grootste bijdrage kwam toen zij in 1843 een artikel van de Italiaanse ingenieur Luigi Menabrea vertaalde over Babbage’ machine. Aan deze vertaling voegde zij uitgebreide eigen notities toe, die uiteindelijk veel langer waren dan het oorspronkelijke artikel. In deze notities beschreef zij een methode om de machine getallen te laten berekenen. <br><br>
+
+
+Wat Ada bijzonder maakte, was haar visie: zij begreep dat computers in de toekomst niet alleen berekeningen zouden uitvoeren, maar ook muziek, tekst en afbeeldingen zouden kunnen verwerken. Deze gedachte was haar tijd ver vooruit.
+<br><br>
+Ada Lovelace overleed op slechts 36-jarige leeftijd aan kanker. Lange tijd bleef haar werk relatief onbekend, maar in de 20e eeuw kreeg zij postuum erkenning als pionier van de informatica. Vandaag de dag wordt zij gezien als een inspiratiebron, vooral voor vrouwen in de technologie en wetenschap.
             </p>
-            <button class="readmore-button-text-introduction-paragraph">
-                Lees hier verder over de erkenning die ze heeft gekregen voor haar werk! 
+            <button class="">
+                Lees hier verder over de erkenning van haar werk.
             </button>
         </div>
-        <div class="image-introduction-container">
-            <img class="image-introduction" src="resources/images/Ada_Lovelace_daguerreotype_by_Antoine_Claudet_1843_-_cropped.png" alt="this is a placeholder image">
-        </div>
+        
     </div>
+    <div class="image-introduction-container">
+            <img class="image-introduction" src="http://blogs.bodleian.ox.ac.uk/adalovelace/wp-content/uploads/sites/163/2018/07/800px-AnalyticalMachine_Babbage_London.jpg" alt="this is a placeholder image">
+        </div>
 </main>
 
 <?php require('partials/footer.php') ?>
