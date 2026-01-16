a221ae93f5eceeb3a2758424a6a3504f8ffa0eaf
kapufskipauw <rosarhode@gmail.com>
2026-01-16 14:19:28 +0100

    fix: Update links biografie page
    Fixed a link that didn't work because it had curly brackets and refactored file for readability.

--- views/biografie.view.php
+++ views/biografie.view.php
@@ -8,22 +8,51 @@
     <div class="introduction-container">
         <div class="text-biografie-container">
             <p class="paragraph-styling">
-                <a class="biografie-link-styling"href="https://en.wikipedia.org/wiki/Ada_Lovelace">Ada Lovelace</a> werd geboren op 10 december 1815 in Piccadilly Terrace, Middlesex (nu: Londen) en overleed op 27 november 1852 in Marylebone, London.
-                Ada was een Engelse wiskundige die samenwerkte met <a class="biografie-link-styling" href="https://www.britannica.com/biography/Charles-Babbage">Charles Babbage</a>, voor zijn prototype digitale computer schreef zij een programma. Ze is benoemd
-                tot de eerste <a class="biografie-link-styling" href="https://wikikids.nl/Programmeur">computerprogrammeur</a>.<br><br>
-                Ada was de dochter van de bekende dichter <a class="biografie-link-styling" href="https://www.britannica.com/biography/Lord-Byron-poet">Heer Byron</a> en <a class="biografie-link-styling" href="https://en.wikipedia.org/wiki/Lady_Byron">Annabella Milbanke</a>, een vrouw met een sterke interesse in wiskunde en logica.
-                Kort na de geboorte van Lovelace gaan haar ouders uit elkaar. Haar vader verliet Engeland voorgoed, dus ze leerde hem nooit persoonlijk kennen. <br><br>Haar moeder stimuleerde haar in de exacte wetenschappen, naar horen en zeggen:
-                'Om te voorkomen dat Ada, net als haar vader, te emotioneel of “dromerig” zou worden.’ Ada bleek al jong talent te hebben voor wiskunde en natuurkunde. Ze kreeg privéles door verschillende tutors, waaronder <a class="biografie-link-styling" href="https://nl.wikipedia.org/wiki/Augustus_De_Morgan">Augustus De Morgan</a>, de eerste professor wiskunde aan de Universiteit van Londen. Op 8 juli 1835 trouwende ze met William King, de 8ste baron King, die in 1839 tot graaf werd benoemd, waardoor Ada de gravin van Lovelace werd. <br><br>
-Lovelace raakte sinds 1833 geinteresseerd in Babbage’s machines nadat zij door 
-<a class="biografie-link-styling" href="https://en.wikipedia.org/wiki/Mary_Somerville"> Mary Somerville </a> en  een bekende wiskundige en wetenschapscommunicator werd voorgesteld aan Charles Babbage. Charles was de ontwerper van de <a class="biografie-link-styling" href=”https://www.britannica.com/technology/Analytical-Engine”> Analytical Engine </a>, een mechanische machine die wordt beschouwd als een voorloper van de moderne computer.<br><br>
-
-
-Ada’s grootste bijdrage kwam toen zij in 1843 een artikel van de Italiaanse ingenieur Luigi Menabrea vertaalde over Babbage’ machine. <a class="biografie-link-styling" href="https://old.maa.org/press/periodicals/convergence/mathematical-treasure-ada-lovelaces-notes-on-the-analytic-engine">Aan deze vertaling voegde zij uitgebreide eigen notities toe </a>, die uiteindelijk veel langer waren dan het oorspronkelijke artikel. In deze notities beschreef zij een methode om de machine getallen te laten berekenen. <br><br>
-
-
-Wat Ada bijzonder maakte, was haar visie: zij begreep dat computers in de toekomst niet alleen berekeningen zouden uitvoeren, maar ook muziek, tekst en afbeeldingen zouden kunnen verwerken. Deze gedachte was haar tijd ver vooruit.
-<br><br>
-Ada Lovelace overleed op slechts 36-jarige leeftijd aan kanker. Lange tijd bleef haar werk relatief onbekend, maar in de 20e eeuw kreeg zij postuum erkenning als pionier van de informatica. Vandaag de dag wordt zij gezien als een inspiratiebron, vooral voor vrouwen in de technologie en wetenschap.
+                <a class="biografie-link-styling"href="https://en.wikipedia.org/wiki/Ada_Lovelace">
+                    Ada Lovelace
+                </a> werd geboren op 10 december 1815 in Piccadilly Terrace, Middlesex (nu: Londen) en overleed op 27 november 1852 in Marylebone, London. Ada was een Engelse wiskundige die samenwerkte met 
+                <a class="biografie-link-styling" href="https://www.britannica.com/biography/Charles-Babbage">
+                    Charles Babbage
+                </a>, voor zijn prototype digitale computer schreef zij een programma. Ze is benoemd tot de eerste 
+                <a class="biografie-link-styling" href="https://wikikids.nl/Programmeur">
+                    computerprogrammeur
+                </a>.
+                <br>
+                <br> Ada was de dochter van de bekende dichter 
+                <a class="biografie-link-styling" href="https://www.britannica.com/biography/Lord-Byron-poet">
+                    Heer Byron
+                </a> en 
+                <a class="biografie-link-styling" href="https://en.wikipedia.org/wiki/Lady_Byron">
+                    Annabella Milbanke
+                </a>, een vrouw met een sterke interesse in wiskunde en logica. Kort na de geboorte van Lovelace gaan haar ouders uit elkaar. 
+                Haar vader verliet Engeland voorgoed, dus ze leerde hem nooit persoonlijk kennen. 
+                <br>
+                <br>Haar moeder stimuleerde haar in de exacte wetenschappen, naar horen en zeggen:'Om te voorkomen dat Ada, net als haar vader, 
+                te emotioneel of “dromerig” zou worden.’ Ada bleek al jong talent te hebben voor wiskunde en natuurkunde. 
+                Ze kreeg privéles door verschillende tutors, waaronder 
+                <a class="biografie-link-styling" href="https://nl.wikipedia.org/wiki/Augustus_De_Morgan">
+                    Augustus De Morgan
+                </a>, de eerste professor wiskunde aan de Universiteit van Londen. Op 8 juli 1835 trouwende ze met William King, de 8ste baron King, 
+                die in 1839 tot graaf werd benoemd, waardoor Ada de gravin van Lovelace werd. 
+                <br>
+                <br> Lovelace raakte sinds 1833 geinteresseerd in Babbage’s machines nadat zij door 
+                <a class="biografie-link-styling" href="https://en.wikipedia.org/wiki/Mary_Somerville">
+                    Mary Somerville 
+                </a> en  een bekende wiskundige en wetenschapscommunicator werd voorgesteld aan Charles Babbage. Charles was de ontwerper van de 
+                <a class="biografie-link-styling" href="https://www.britannica.com/technology/Analytical-Engine"> 
+                    Analytical Engine 
+                </a>, een mechanische machine die wordt beschouwd als een voorloper van de moderne computer.
+                <br>
+                <br> Ada’s grootste bijdrage kwam toen zij in 1843 een artikel van de Italiaanse ingenieur Luigi Menabrea vertaalde over Babbage’ machine. 
+                <a class="biografie-link-styling" href="https://old.maa.org/press/periodicals/convergence/mathematical-treasure-ada-lovelaces-notes-on-the-analytic-engine">
+                    Aan deze vertaling voegde zij uitgebreide eigen notities toe 
+                </a>, die uiteindelijk veel langer waren dan het oorspronkelijke artikel. In deze notities beschreef zij een methode om de machine getallen te laten berekenen. 
+                <br>
+                <br> Wat Ada bijzonder maakte, was haar visie: zij begreep dat computers in de toekomst niet alleen berekeningen zouden uitvoeren, maar ook muziek, tekst en afbeeldingen 
+                zouden kunnen verwerken. Deze gedachte was haar tijd ver vooruit.
+                <br>
+                <br>Ada Lovelace overleed op slechts 36-jarige leeftijd aan kanker. Lange tijd bleef haar werk relatief onbekend, maar in de 20e eeuw kreeg zij postuum erkenning als pionier van de informatica. 
+                Vandaag de dag wordt zij gezien als een inspiratiebron, vooral voor vrouwen in de technologie en wetenschap.
             </p>
             <button class="">
                 Lees hier verder over de erkenning van haar werk.
