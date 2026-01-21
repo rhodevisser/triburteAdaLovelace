0184dd7924cadd48a445ecf6a8abc42e27c91036
kapufskipauw <rosarhode@gmail.com>
2026-01-21 19:01:37 +0100

    style: Added styling to erkenning page

--- views/erkenning.view.php
+++ views/erkenning.view.php
@@ -6,22 +6,33 @@
 
 <main>
     <div class="introduction-container">
-        <div class="text-introduction-container">
+        <div class="text-erkenning-container">
             <p class="paragraph-styling">
                 Tijdens haar leven kreeg Ada Lovelace nauwelijks erkenning voor haar werk.  Dit had meerdere redenen. Om te beginnen werd de Analyitical Engine, waarvoor Ada het algoritme schreef, nooit gebouwd. Hierdoor bleef haar werk lange tijd theoretisch. Daarnaast was de wetenschap in de negentiende eeuw een mannendomein. Het werk van vrouwen werd vaak ondergebracht onder de paraplu van een man die vervolgens de erkenning van het werk kreeg.
+            </p> 
 
+            <h2 class="header-two-styling">
                 1950s: Een centrale plek binnen de geschiedenis van de computerwetenschap. 
             </h2> 
+            <p class="paragraph-styling">
                 In de jaren 1950 schreef Alan Turing, ook wel bekend als de vader van computerwetenschap, het artikel ‘Computing Machinery and Intelligence’, waarin hij de haalbaarheid van kunstmatige intelligentie onderzocht. Later stond die artikel bekend als de ‘Turing test’. In zijn artikel verwijs hij naar wat hij “Lady Lovelace’s objection” noemde. Lovelace’ stelling was dat de Alalytical Engine enkel taken kon uitvoren, en zelf niks nieuws kon uitbrengen. In zijn artikel gebruikte hij haar stelling als tegenargument in zijn betoog over denkende machines. Sindsdien kreeg haar werk een centrale plaats binnen de geschiedenis van de computerwetenschap.
+            </p>
 
+            <h2 class="header-two-styling">
                 1980s: Programmeertaal Ada 
+            </h2>
+            <p class="paragraph-styling">
                 In 1980 werd de programmeertaal vernoemd naar Ada waarmee haar nalatenschap een blijvende plek kreeg binnen de informatica. De taal Ada verving zo’n 450 verschillende programmeertalen die door het Armerikaanse leger werden gebruikt.
+            </p>
 
+            <h2 class="header-two-styling">
                 Ada Lovelace Day
+            </h2>
+            <p class="paragraph-styling">
                 Sinds oktober 2009 wordt daarnaast jaarlijks Ada Lovelace Day georganiseerd, een initiatief van activiste Suw Charman-Anderson. Deze dag vindt plaats op de tweede dinsdag van oktober en is bedoeld om de prestaties van vrouwen in de wetenschap, technologie, engineering en wiskunde (STEM) zichtbaar te maken. In het STEM gebied zijn hun bijdragen historisch gezien vaak onderbelicht gebleven. Ada Lovelace Day wordt gevierd door middel van uiteenlopende activiteiten, zoals evenementen op scholen, in academische instellingen, bibliotheken en musea, met name in de Verenigde Staten en het Verenigd Koninkrijk. Onderdeel hiervan zijn onder andere Wikipedia-edit-a-thons, die gericht zijn op het vergroten van de zichtbaarheid van vrouwen in STEM, evenals filmvertoningen en paneldiscussies.
             </p>
-            <button class="readmore-button-text-introduction-paragraph">
-                Vind hier bronnen om meer te leren over Ada Lovelave of the imacpat van vrouwn in tech
+            <button>
+                Lees verder ver initaiteven voor vrouwn in tech
             </button>
         </div>
         <div class="image-introduction-container">
