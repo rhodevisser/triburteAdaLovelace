f2432812ac40d39c96fba80ee058e02ed346e4a2
kapufskipauw <rosarhode@gmail.com>
2026-01-21 16:46:39 +0100

    feat: Added 404 file
    If there is a weird link used on the website it will show the 404 page

--- router.php
+++ router.php
@@ -20,4 +20,12 @@ function routeToController($uri, $routes) {
 }
 
 
+function abort($code = 404) {
+    http_response_code($code); 
+
+    require "views/{$code}.php";
+
+    die();
+} 
+
 routeToController( $uri, $routes);
---
+++ views/404.php
@@ -0,0 +1,16 @@
+<?php require('partials/head.php') ?>
+
+<?php require('partials/nav.php') ?>
+
+<main>
+    <div class="introduction-container">
+        <div class="text-biografie-container">
+           <h1> 404 </h1>
+            <button>
+                Ga terug naar home
+            </button>
+        </div>
+        
+</main>
+
+<?php require('partials/footer.php') ?>
