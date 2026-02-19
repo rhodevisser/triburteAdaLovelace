11620f868cd9c843e9514305f445724efda30565
kapufskipauw <rosarhode@gmail.com>
2026-02-19 19:33:39 +0100

    create: Added config.php DB connection
    Created mysql in TablePlus and added DB values to config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'port' => '3306', 
+        'dbname' => 'girlsresourcesDB', 
+        'charset' => 'utf8mb4'
+    ]
+];
+
